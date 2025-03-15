09034a325d7cd9d1583dc04b529982697fe400a9
sewaq <109106122+sewaqbhawiq98@users.noreply.github.com>
2025-03-15 10:16:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Portfolio Loaded!");
+});
+function scrollToAbout() {
+    document.getElementById("about").scrollIntoView({ behavior: "smooth" });
+}
